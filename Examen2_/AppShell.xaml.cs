96633c49ff7e600acc7061a2a3be339b841710d0
Melanie Ramos <Melanie Ramos@LAPTOP-6FN43BTQ>
2025-06-27 10:49:24 -0600

    Agregar archivos de proyecto.

---
+++ Examen2_/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Examen2_
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Examen2_/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Examen2_
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
